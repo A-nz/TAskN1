b7a25f395277069192c72b0de663d08991a296fa
Petrov Andrey <pandr2002@mail.ru>
2020-10-22 10:23:18 +0300

    .

--- src/ru/vsu/cs/Main.java
+++ src/ru/vsu/cs/Main.java
@@ -3,39 +3,39 @@ package ru.vsu.cs;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args){
-        double t1= GetValue("температура 1");
-        double t2= GetValue("температура 2");
-        double v1= GetValue("объём 1");
-        double v2= GetValue("объём 2");
+    public static void main(String[] args) {
+        double t1 = getValue("температура 1");
+        double t2 = getValue("температура 2");
+        double v1 = getValue("объём 1");
+        double v2 = getValue("объём 2");
 
-        double volume = MixVolume(v1, v2);
-        double temp = MixTemp(t1, t2);
-        PrintMixTempAndMixVolume(volume, temp);
+        double volume = mixVolume(v1, v2);
+        double temp = mixTemp(t1, t2);
+
+        printValue("объем смеси", volume);
+        printValue("температура смеси", temp);
     }
 
-    private static double MixTemp (double t1, double t2) {
-        return ((t1+t2) / 2);
+    private static double mixTemp(double t1, double t2) {
+        return ((t1 + t2) / 2);
     }
 
-    private static double MixVolume (double v1, double v2) {
-        return (v1+v2);
+    private static double mixVolume(double v1, double v2) {
+        return (v1 + v2);
     }
 
-    private static double GetValue(String NameOfValue) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.printf("%s=", NameOfValue);
+    private static double getValue(String NameOfValue) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("%s = ", NameOfValue);
         return scanner.nextDouble();
 
     }
 
-    private static void PrintMixTempAndMixVolume(double volume, double temp) {
-        System.out.println("Объём смеси равен: " + volume);
-        System.out.println("Температура смеси равна: " + temp);
+    private static void printValue(String name, double x) {
+        System.out.printf("%s = %.1f%n", name, x);
     }
 
 }
-
 
 
 
